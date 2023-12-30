b21b38e99756efd631927d83e43a3888b7e286b9
dddhafin <dhafin@ddhafin.userbejir>
2023-12-30 17:21:15 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],  theme: {
+    extend: {
+      animation: {
+        typewriter: 'typewriter 1s steps(20) forwards',
+        caret: 'typewriter 1s steps(30) forwards, blink 1s steps(11) infinite 2s',
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: '100%',
+          },
+        },
+        blink: {
+          '0%': {
+            opacity: '0',
+          },
+          '0.1%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
